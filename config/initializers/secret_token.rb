9c37a1ac13bcbfc3af4346a074bb499c66edb477
WolfLee <liyuan0228@gmail.com>
2013-12-09 17:10:28 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jobvious::Application.config.secret_key_base = '0557dd5e2df950a0eed0270a0c16985a066abd2b215e39157521df957bc98daa51816972f07a37cd5ca7a46b785067fb0d05523d9785ffdef9530760dad26bb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jobvious::Application.config.session_store :cookie_store, key: '_jobvious_session'
