3ff0ae06e63a0a3ef4a93e43dc8533b96ac24fce
WolfLee <liyuan0228@gmail.com>
2013-12-14 00:42:02 +0800

    Bug Fix
    Fix an issue when login with linkedin causes 500 Interal Sever Error.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131211161100) do
+ActiveRecord::Schema.define(version: 20131213130504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authorizations", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.string   "username"
+    t.string   "token"
+    t.string   "secret"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +41,7 @@ ActiveRecord::Schema.define(version: 20131211161100) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
