fa4c80d2c883f97c5faef8a412cc41ac830e37ee
WolfLee <liyuan0228@gmail.com>
2013-12-25 18:02:08 +0800

    Add job full text search feature
    https://trello.com/c/ALWkmuJp
    
    Anyone from the homepage and the jobs page (which is the same
    technically) can see a search bar and searh any job keywords. Supporting
    'AND', 'OR' and 'NOT', it is powered by elasticsearch natively.
    Highlighting is commented out now (and not enabled on views by default).

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,4 +6,35 @@ class Job < ActiveRecord::Base
   validates :description, presence: true
   validates :contact, presence: true
   validates :company, presence: true
+
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
+  def self.search(params)
+    tire.search(load: true) do
+      query { string params[:q] } if params[:q].present?
+      #not quite working here, tire to blame
+      #highlight :title, :description, options: { tag: '<strong class="highlight">' }
+      sort { by :created_at, 'desc' }
+      page = (params[:page] || 1).to_i
+      search_size = params[:per_page] || 20
+      from (page - 1) * search_size
+      size search_size
+    end
+  end
+
+  after_save do
+    tire.update_index
+  end
+
+  mapping do
+    indexes :id,          index: :not_analyzed
+    indexes :title,       type: 'string', analyzer: 'snowball'
+    indexes :location,    type: 'string', analyzer: 'snowball'
+    indexes :description, type: 'string', analyzer: 'snowball'
+    indexes :contact,     type: 'string', analyzer: 'snowball'
+    indexes :company,     type: 'string', analyzer: 'snowball'
+    indexes :url,         type: 'string', analyzer: 'simple'
+    indexes :created_at,  type: 'date',   include_in_all: false
+  end
 end
